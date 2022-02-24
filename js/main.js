fa476159e72318864fee22befc837bc426c13df6
Karina <Hogwartslumusmaxima@internet.ru>
2022-02-24 21:57:59 +0600

    новый код

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 "use strict";
 
 const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
+    { id: 1, title: 'Notebook', price: 2000},
+    { id: 2, title: 'Mouse', price: 20},
     { id: 3, title: 'Keyboard', price: 200 },
     { id: 4, title: 'Gamepad', price: 50 },
 ];
+
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+//добавила изображение по умолчанию
+const renderProduct = (title, price, image="01.jpg") => {
     return `<div class="product-item">
+                <img src=${image} alt="">
                 <h3>${title}</h3>
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
+
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item.title, item.price, item.image));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
+
+//innerHTML вызывает метод ToString , это тоже самое что и join(',');
+//надо добавить еще join(''), чтобы запятая исчезла
 
 renderPage(products);
