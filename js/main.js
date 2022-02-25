dbf8365dd9c9060d767d0aa56f30139b694f7591
Karina <Hogwartslumusmaxima@internet.ru>
2022-02-25 20:07:55 +0600

    код в комментарии дест-ия

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 "use strict";
 
 const products = [
-    { id: 1, title: 'Notebook', price: 2000},
-    { id: 2, title: 'Mouse', price: 20},
+    { id: 1, title: 'Notebook', price: 2000,},
+    { id: 2, title: 'Mouse', price: 20,},
     { id: 3, title: 'Keyboard', price: 200 },
     { id: 4, title: 'Gamepad', price: 50 },
 ];
-
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 //добавила изображение по умолчанию
+
+// let{title, price} = obj; не сработал
+
 const renderProduct = (title, price, image="01.jpg") => {
     return `<div class="product-item">
                 <img src=${image} alt="">
