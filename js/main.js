a1cd30d41c68581e9edb121e2d8b3e6fb5124584
Karina <Hogwartslumusmaxima@internet.ru>
2022-03-04 20:43:44 +0600

    modified file js

--- js/main.js
+++ js/main.js
@@ -1,76 +1,81 @@
-"use strict";
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class ProductList{
-    constructor(container='.products'){
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     // window.ActiveXObject -> xhr = new ActiveXObject()
+//     xhr.open("GET", url, true);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status !== 200){
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
+class ProductsList {
+    constructor(container = '.products'){
         this.container = container;
-        this.goods = [];
-        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
-        this.render();//вывод товаров на страницу
-        this.sum = 0;
-        this.getSum();
-        this.showSum();
+        this.goods = [];//массив товаров из JSON документа
+        this._getProducts()
+            .then(data => { //data - объект js
+                 this.goods = data;
+//                 console.log(data);
+                 this.render()
+            });
     }
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+    // _fetchProducts(cb){
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this.goods = JSON.parse(data);
+    //         console.log(this.goods);
+    //         cb();
+    //     })
+    // }
+    _getProducts(){
+      
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+       
     }
-    //Пример взяла напрямую с factor завода машин, сделала 2 функции, одна считает сумму, другая показывает в консоль
-    getSum() {
-         for(let i = 0;i < this.goods.length;i++) {
-           this.sum += this.goods[i].price;
-        }
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
-
-    showSum() {
-        console.log(`Общая сумма всех товаров ${this.sum}`);
-    }
-    
     render(){
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
-             const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-//           block.innerHTML += item.render();
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+//            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
         }
+
     }
 }
 
-class ProductItem{
-    constructor(product,img= '01.jpg'){
-        this.title = product.title;
-        this.id = product.id;
+
+class ProductItem {
+    constructor(product, img = '01.jpg'){
+        this.title = product.product_name;
         this.price = product.price;
+        this.id = product.id_product;
         this.img = img;
     }
     render(){
-           return `<div class="product-item">
-                <img src="${this.img}">
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-                <button class="buy-btn">Купить</button>
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
             </div>`
     }
 }
 
-let list = new ProductList();
-
-//для Корзины сделала метод сделать заказ и удалить все
-class Cart {
-    doOrder(){
-    };
-    removeAll(){
-    };
-}
-
-// для Элемента корзины товара, сделала методы удалить(сам товар удаляется)
-// и добавить товар(можно кол-во сделать больше)
-class GoodOfCart {
-    remove() {
-    };
-    add(){
-    };
-}
+let list = new ProductsList();
+console.log(list.allProducts);
